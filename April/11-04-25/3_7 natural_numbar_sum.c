3ee8ff7bb6e919e6834bd06de14f08daa08ec696
mobarak19-web <mdmobarokhosini0203@gmail.com>
2025-04-12 10:13:35 +0600

    solving for w3schools  problem numbar 3  for loop

---
+++ April/11-04-25/3_7 natural_numbar_sum.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int i,sum=0;
+
+    printf("fist 7natural numbar display:\n");
+    for(i=1;i<=7;i++){
+        sum=sum+i;
+   printf("%d\n",i);
+  
+    }
+    printf("the sum value:%d\n",sum);
+
+    return 0;
+}
