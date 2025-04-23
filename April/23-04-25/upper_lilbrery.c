59731fda8001346b1bd32db1c219a22cb4de776e
mobarak19-web <mdmobarokhosini0203@gmail.com>
2025-04-23 20:15:49 +0600

    uppercase letter to lowercase letter usinf lilbrery function

---
+++ April/23-04-25/upper_lilbrery.c
@@ -0,0 +1,10 @@
+//to(lilbrery function) upper function apply
+#include<stdio.h>
+int main(){
+    char upper,lower;
+    printf(" Enter the loweercase letter : ");
+    scanf("%c",& upper);
+    lower =tolower(upper);
+     printf(" Enter lowercase letter : %c",lower);
+     return 0;
+}
