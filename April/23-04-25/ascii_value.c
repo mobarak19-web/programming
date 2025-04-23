12b200f22d96cbc06946e0a38fd665b58d7efe2c
mobarak19-web <mdmobarokhosini0203@gmail.com>
2025-04-23 19:07:36 +0600

    ascii value to ascii charecter return

---
+++ April/23-04-25/ascii_value.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+   int numbar;
+   printf("Enter the ascii value : ");
+   scanf("%d",&numbar);
+   printf(" Enter the ascii charecter : %c\n",numbar);
+   
+
+    return 0;
+}
