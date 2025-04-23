443ea867d4fa5db015320f0f5c6a01cff2d46481
mobarak19-web <mdmobarokhosini0203@gmail.com>
2025-04-23 20:25:22 +0600

    decimal to octal canversion

---
+++ April/23-04-25/decimal.c
@@ -0,0 +1,10 @@
+//decimal to octal canversion
+#include<stdio.h>
+int main(){
+    int num;
+    printf(" Enter the decimal numbar : ");
+    scanf("%d",&num);
+
+    printf(" Enter the octal numbar :%o",num);
+    return 0;
+}
