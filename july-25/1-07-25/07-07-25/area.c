c792368d78a84e4fa5700694c0b776c2bee52b6c
mobarak19-web <mdmobarokhosini0203@gmail.com>
2025-07-07 20:10:15 +0600

    Finding the area of ​​a triangle by the lengths of its 3 sides

--- july-25/1-07-25/07-07-25/area.c
+++ july-25/1-07-25/07-07-25/area.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
 int main(){
-int  a,b,c;
-float avg;
+int  a,b,c,s;
+float are;
 
 printf("Enter  thr three  numbar: ");
 scanf("%d%d%d",&a,&b,&c);
 
-avg=(a+b+c)/2;
-
-printf("the avg is =%.2f\n",avg);
+s=(a+b+c)/2;
+are=sqrt (s*(s-a)*(s-b)*(s-c));
+//printf("the sum is =%d\n",s);
+printf("the area is =%.2f\n",are);
 return 0;
 }
