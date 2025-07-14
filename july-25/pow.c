5885148e041163e86cbe11177cab9710979dee06
mobarak19-web <mdmobarokhosini0203@gmail.com>
2025-07-14 21:35:26 +0600

    The power of x raised to y

--- july-25/pow.c
+++ july-25/pow.c
@@ -5,6 +5,6 @@ int main() {
     printf("Enter the base and exponent: ");
     scanf("%d %d", &x, &y);
     double result=pow(x, y);
-    printf("The power of 2 raised to 3 is: %.2lf\n", result);
+    printf("The power of x raised to y is: %.2lf\n", result);
     return 0;
 }
