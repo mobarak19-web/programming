24b4bfb46d8495e12d6b0486d1cbd098284e35ea
mobarak19-web <mdmobarokhosini0203@gmail.com>
2025-08-04 09:29:36 +0600

    Sum of N Natural Numbers

---
+++ August--25/04-08-25/1 TO 20 PLM/sumofnatural.c
@@ -0,0 +1,23 @@
+/*10. Sum of N Natural Numbers
+Description: Find sum of first N natural numbers.
+
+Input: 10
+
+Output: 55
+
+Formula: n(n+1)/2
+
+Test Cases:
+*/
+#include<stdio.h>
+int main(){
+    int n,sum=0,i;
+    printf("input :");
+    scanf("%d",&n);
+    for( i=1; i<=n; i++){
+        printf(" %d\n",i);
+       sum=sum +i;
+    }
+       printf("Output :%d\n", sum);
+    return 0;
+}
