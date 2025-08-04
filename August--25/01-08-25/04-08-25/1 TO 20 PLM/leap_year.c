2c74ae32a93ce38bdeefa3e8d037bf37902c7c93
mobarak19-web <mdmobarokhosini0203@gmail.com>
2025-08-04 08:47:10 +0600

    A number is even or odd.

---
+++ August--25/01-08-25/04-08-25/1 TO 20 PLM/leap_year.c
@@ -0,0 +1,13 @@
+/*8. Leap Year Checker
+Description: Check if a given year is a leap year.
+
+Condition: Year divisible by 4 and (not divisible by 100 unless divisible by 400)
+
+Input: 2020
+
+Output: Leap Year
+*/
+#include<stdio.h>
+int main(){
+    int 
+}
