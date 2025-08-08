2f202eddca7b53eace567cb968ab39d85ce8358e
mobarak19-web <mdmobarokhosini0203@gmail.com>
2025-08-09 01:46:39 +0600

    Array Store & Print

---
+++ August--25/07-08-25/array_print.c
@@ -0,0 +1,30 @@
+/*1. Array Store & Print
+Write a program in C to store elements in an array and print them.
+Test Data :
+Input 10 elements in the array :
+element - 0 : 1
+element - 1 : 1
+element - 2 : 2
+.......
+Expected Output :
+Elements in array are: 1 1 2 3 4 5 6 7 8 9
+*/
+#include<stdio.h>
+int main(){
+    int arr[100],num;
+    arr[0]=1;
+    arr[1]=1;
+    printf("How many numbers :");
+    scanf("%d",&num);
+    /*  printf("Enter the  number :\n");
+     for(int i=0; i<num; i++){
+        scanf("%d",arr[i]);
+     }*/
+     for(int i=2; i<num; i++){
+      arr[i]=arr[i-1] + arr[i-2];
+     }
+     for(int i=0;i<num; i++){
+      printf(" %d",arr[i]);
+     }
+     return 0;
+}
