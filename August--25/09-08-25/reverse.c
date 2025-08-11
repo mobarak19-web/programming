55e5270592654e0da227262ae161e2f5ccd5f1d8
mobarak19-web <mdmobarokhosini0203@gmail.com>
2025-08-11 12:53:54 +0600

    reverse in the array

---
+++ August--25/09-08-25/reverse.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+    int arr[100];
+    int i,num;
+
+     printf("Elements to store in the array :");
+       scanf("%d", &num );
+    printf("Input %d number of elements in the array :\n", num);
+    for(i=0; i<num; i++){
+        printf("element - %d : ", i);
+        scanf(" %d",&arr[i]);
+    }
+    printf(" \n Elements in array are : ");
+    for(i=0; i<num; i++){
+        printf(" %d",arr[i]);
+    }
+    printf("\n");
+    printf(" reverse in the array : ");
+    for(i=num-1; i>=0; i--)
+    {
+      printf(" %d",arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
