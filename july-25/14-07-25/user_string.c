7db9d072273a0c13fe8114cf58afec0304100bfc
mobarak19-web <mdmobarokhosini0203@gmail.com>
2025-08-14 22:58:24 +0600

    user input by string

---
+++ july-25/14-07-25/user_string.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    char s1[100]; //={'m','o','b','a','r','a','k'};
+  printf(" Enter  your full name : ");
+  gets(s1);
+  printf(" Full Name = %s\n", s1);
+ return 0;
+}
