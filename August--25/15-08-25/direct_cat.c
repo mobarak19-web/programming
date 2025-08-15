cb1d0db98e984bbaf97934a21067cdb107107c1c
mobarak19-web <mdmobarokhosini0203@gmail.com>
2025-08-15 21:56:30 +0600

    without cat function

---
+++ August--25/15-08-25/direct_cat.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+  char str1[]={" my name : "};
+  char str2[]={ "Mobarak husini "};
+
+  int i=0, len=0;
+  while(str1[i] !='\0'){
+    i++;
+    len++;
+  }int j=0;
+  while(str2[j] !='\0'){
+    str1[len+j] = str2[j];
+    j++;
+  }
+
+  printf(" str1 = %s\n",str1);
+  return 0;
+
+}
