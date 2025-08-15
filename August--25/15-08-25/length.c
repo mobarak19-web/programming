a8df3cb0ff6d6e0974996117bd4bef0b7d0e7b6e
mobarak19-web <mdmobarokhosini0203@gmail.com>
2025-08-15 22:01:50 +0600

    strlen function used = length

---
+++ August--25/15-08-25/length.c
@@ -0,0 +1,8 @@
+// strlen function bebohar kore length ber korte hoy 
+#include<stdio.h>
+int main(){
+    char str[]={"mobarak husini"};
+    int len =strlen(str);
+    printf("Length = %d\n", len);
+    return 0;
+}
