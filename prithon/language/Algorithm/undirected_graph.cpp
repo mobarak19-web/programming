0f265eb16278dec50b4318675f4db5af26e44e5b
mobarak19-web <mdmobarokhosini0203@gmail.com>
2026-04-13 20:03:40 +0600

    undirected graph algorithm

--- prithon/language/Algorithm/undirected_graph.cpp
+++ prithon/language/Algorithm/undirected_graph.cpp
@@ -1,4 +1,26 @@
 #include<bits/stdc++.h>
 #include<vector>
 using namespace std;
- 
+  const int MAXN =100;
+ int main(){
+    int n,e;
+    cin >> n >> e;
+    vector<int> adjlist[n+1];
+    
+    for(int i=0; i<e; i++){
+        int v,u;
+        cin >> v >> u;
+        adjlist[v].push_back(u);
+        adjlist[u].push_back(v);
+
+    }
+    for(int i=1; i<=n; i++){
+        cout << i << " -->";
+        for(int j : adjlist[i]){
+            cout << j << " ";
+            
+        }
+        cout << '\n';
+    }
+    return 0;
+ }
