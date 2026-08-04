c31b08ab801d9cb830291389a1a1a9e23dfe9fed
mobarak19-web <mdmobarokhosini0203@gmail.com>
2026-08-05 01:46:42 +0600

    learning how to take input in python

--- python/first.py
+++ python/first.py
@@ -1,5 +1,7 @@
 
 name="mobarak"
 age=24
+cgpa=3.49
 print(type(name))
 print(type(age))
+print(type(cgpa))
---
+++ python/input.py
@@ -0,0 +1,3 @@
+# learning how to take input in python
+name=input("Enter your nsme:")
+print("Hell0 " + name)# concatanation used +name ;
