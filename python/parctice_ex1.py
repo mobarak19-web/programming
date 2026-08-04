cb72512c8e3c541ec196524104b299a48afa0a3a
mobarak19-web <mdmobarokhosini0203@gmail.com>
2026-08-05 02:59:26 +0600

    print suoerhero name and age in python

---
+++ python/parctice_ex1.py
@@ -0,0 +1,13 @@
+#add a program with first name as tony and last name stark
+#tony's age is 53
+#tony's height is 1.85m
+#
+name="Tony stack"
+age=53
+height= 1.85
+superhero=input("Enter Tony's your name :")
+
+print(name)
+print(age)
+print(height)
+print(superhero)
---
+++ python/sum_mul_div.py
@@ -0,0 +1,10 @@
+a=int(input("Enter a:"))
+b=int(input("Enter b:"))
+
+sum= a+b
+mul=a*b
+div=a/b
+
+print("Sum",sum)
+print("Mulpti",mul)
+print("Div",div)
