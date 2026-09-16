8f9b836ca209974b279019f67872dea154155c7a
Mobarak <mdmobarokhosini0203@gmail.com>
2026-09-16 18:31:53 +0600

    all list method

---
+++ python/list_method.py
@@ -0,0 +1,19 @@
+list = [2, 1, 3]
+
+li = [3, 8, 4, 1]
+
+list.append(4)  # adds one element at the end [2,1,3,4]
+
+list.sort()   #sorts in ascending order [1,2,3,4]
+
+
+li.sort(reverse=True)  # sort in dcsending order [8,4,3,1]
+
+# li.reverse()     reverse print now 
+
+print(li)
+
+
+list.insert(2, 56)  #insert element at index 
+
+print(list)
